855db082b420f17ed0c27bb5feb87e24e735b5aa
Ray <lanskyassassination@gmail.com>
2024-08-05 09:56:58 +0800

    chore: Update webpack configuration and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,48 @@
-const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = (env, argv) => {
-    const isProduction = argv.mode === "production";
+	const isProduction = argv.mode === 'production';
 
-    return {
-        mode: isProduction ? "production" : "development",
-        entry: {
-            popup: "./src/popup.js",
-            background: "./src/background.js",
-        },
-        output: {
-            path: path.resolve(__dirname, "dist"),
-            filename: "[name].js",
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["@babel/preset-env"],
-                        },
-                    },
-                },
-                {
-                    test: /\.css$/,
-                    use: ["style-loader", "css-loader"],
-                },
-            ],
-        },
-        plugins: [
-            new CleanWebpackPlugin(),
-            new CopyWebpackPlugin({
-                patterns: [
-                    { from: "src/popup.html", to: "" },
-                    { from: "src/icons", to: "icons" },
-                    { from: "src/manifest.json", to: "" },
-                ],
-            }),
-        ],
-        devtool: isProduction ? false : "source-map",
-    };
+	return {
+		mode: isProduction ? 'production' : 'development',
+		entry: {
+			popup: './src/popup.js',
+			background: './src/background.js',
+		},
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: '[name].js',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env'],
+						},
+					},
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader'],
+				},
+			],
+		},
+		plugins: [
+			new CleanWebpackPlugin(),
+			new CopyWebpackPlugin({
+				patterns: [
+					{ from: 'src/popup.html', to: '' },
+					{ from: 'src/icons', to: 'icons' },
+					{ from: 'src/manifest.json', to: '' },
+				],
+			}),
+		],
+		devtool: isProduction ? false : 'source-map',
+	};
 };
